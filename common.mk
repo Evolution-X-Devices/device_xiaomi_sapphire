d224f82dc7d9eab4995983d5b813b1baa7211535
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-04-25 02:48:36 +0200

    sm6225-common: Disable UDFPS for topaz
    Change-Id: I71523b61b47cb2d454d38ba5ba8aeb08e7b93d11

--- common.mk
+++ common.mk
@@ -219,8 +219,12 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.xiaomi \
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi
+
+ifeq ($(TARGET_HAS_UDFPS),true)
+PRODUCT_PACKAGES += \
     libudfpshandler
+endif
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
