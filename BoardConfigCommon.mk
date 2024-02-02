ce427addc34c752e52d7d3397bfc3a1188ba9cc4
Luofan Chen <clfbbn@gmail.com>
2024-02-02 20:49:44 +0800

    sm6225-common: Import QCC system_ext blobs
    Makes the QTI GNSS service happy. Thanks luca020400 for the help.
    
    Change-Id: I6de25b0b3fde6cce3a6cdf95f31c68734d429956

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -238,6 +238,7 @@ BOARD_AVB_ODM_ADD_HASHTREE_FOOTER_ARGS += --hash_algorithm sha256
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/configs/vintf/framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
+DEVICE_FRAMEWORK_MANIFEST_FILE += $(COMMON_PATH)/configs/vintf/framework_manifest.xml
 DEVICE_MANIFEST_FILE += \
     $(COMMON_PATH)/configs/vintf/manifest.xml \
     $(COMMON_PATH)/configs/vintf/network_manifest.xml
