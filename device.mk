858a1ec7e9fa7b7434d027beeecad0789a1a223b
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-02-14 09:25:55 -0600

    sapphire: rootdir: Dynamically set IRQ affinity for KGSL and MSM_DRM
    - Introduce a new shell script to set IRQ affinity, since different
      kernels can have different IRQ numbers.
    
    Change-Id: I61d2350e6b40f096714148c568489579bc4d2371
    Co-Authored-By: Alex Naidis <alex.naidis@paranoidandroid.co>
    Co-Authored-By: Jake Weinstein <jake@aospa.co>

--- device.mk
+++ device.mk
@@ -318,6 +318,7 @@ PRODUCT_PACKAGES += \
     init.qti.early_init.sh \
     init.qti.kernel.sh \
     init.qti.write.sh \
+    init.sapphire_perf.sh \
     system_dlkm_modprobe.sh \
     vendor_modprobe.sh
 
