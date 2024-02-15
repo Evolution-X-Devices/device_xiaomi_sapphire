9ee1cde16e4169adce7c93c4d916d384f513cbb4
=Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>
2024-02-15 11:53:55 +0000

    sm6225-common: Enable ADB by default in non-user builds
    Change-Id: I35989e482ec04993ae1a9ecfc682cafdd70e8c52
    Signed-off-by: ryzenforce990 <ryzenforce990@gmail.com>

--- common.mk
+++ common.mk
@@ -549,6 +549,11 @@ PRODUCT_PACKAGES_DEBUG += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.2.vendor:64
 
+ifneq ($(TARGET_BUILD_VARIANT),user)
+PRODUCT_VENDOR_PROPERTIES += \
+    persist.vendor.usb.config=mtp,adb
+endif
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
