a87118fc4173605bf6a218f9bc6287fc7aabd7f1
basamaryan <basam.aryan@gmail.com>
2024-03-05 19:59:05 +0700

    sm6225-common: Forcefully disable volume leveler
    * Taken from https://github.com/PixelOS-Devices/device_oneplus_sm8350-common/commit/6ef2aced0c6bdd5cc9060a85a1fee955f7b3db46#diff-e13df962209619006265999415c36ff216d10631983ebfe8ca6d822f8314ffb6R66-R68
    
    Change-Id: I7ceb29291a9e55036d57975f5a28a149c7fcb435

--- extract-files.sh
+++ extract-files.sh
@@ -66,6 +66,9 @@ function blob_fixup() {
         vendor/lib*/soundfx/libdlbvol.so|vendor/lib*/soundfx/libhwdap.so|vendor/lib64/libdlbdsservice.so | vendor/lib64/libcodec2_soft_ac4dec.so | vendor/lib64/libcodec2_soft_ddpdec.so)
             "${PATCHELF}" --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
             ;;
+        vendor/etc/dolby/dax-default.xml)
+            sed -i "/volume-leveler-enable/ s/true/false/g" "${2}"
+            ;;
     esac
 }
 
