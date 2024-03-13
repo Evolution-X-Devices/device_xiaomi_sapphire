4485b1c99a346a17c9c6706f2bdf704a5c64440d
boedhack99 <boedhack@gmail.com>
2024-03-13 21:43:40 +0700

    sm6225-common: Switch to use stock powerhint.xml * from topaz V816.0.2.0.UMGMIXM
    Change-Id: Iff7f2716ddbf338d880f41207dce031eab4301a6

--- common.mk
+++ common.mk
@@ -437,7 +437,7 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.2.vendor
 
 PRODUCT_COPY_FILES += \
-    vendor/qcom/opensource/power/config/bengal/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+    $(LOCAL_PATH)/configs/perf/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
 # QCC
 PRODUCT_PACKAGES += \
